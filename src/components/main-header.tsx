3dde9736cf5fc43dbbca86c5d12b40aae5f532a1
leviettien1808 <leviettien1808@gmail.com>
2025-10-21 21:36:00 +0700

    [component] Update: Background to MainHeader

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -3,29 +3,33 @@ import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
 import classes from "@/components/main-header.module.css";
+import MainHeaderBackground from "@/components/main-header-background";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image
-          src={logoImg}
-          alt="A red orange food cloche, crossed fork and spoon, FOOD text below"
-          priority
-        />
-        Next Logo
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImg}
+            alt="A red orange food cloche, crossed fork and spoon, FOOD text below"
+            priority
+          />
+          Next Logo
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
