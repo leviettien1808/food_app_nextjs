f12284686ace206f009066ae6e220938141f2ddb
leviettien1808 <leviettien1808@gmail.com>
2025-10-21 09:41:09 +0700

    [style] CSS module:  MainHeader

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
 
 import logoImg from "@/assets/logo.png";
+import classes from "@/components/main-header.module.css";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
         <img
           src={logoImg.src}
           alt="A red orange food cloche, crossed fork and spoon, FOOD text below"
         />
+        Next Logo
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
