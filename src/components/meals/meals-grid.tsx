67b01ce1db33a0dcc363a5f501c91cbad22bfe53
leviettien1808 <leviettien1808@gmail.com>
2025-10-23 11:23:11 +0700

    [component] Add new: MealsGrid

---
+++ src/components/meals/meals-grid.tsx
@@ -0,0 +1,25 @@
+import classes from "@/components/meals/meals-grid.module.css";
+import MealItem from "@/components/meals/meal-item";
+
+type MealsGridProps = {
+  meals: {
+    id: string;
+    title: string;
+    slug: string;
+    image: string;
+    summary: string;
+    creator: string;
+  }[];
+};
+
+export default function MealsGrid({ meals }: MealsGridProps) {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
+    </ul>
+  );
+}
