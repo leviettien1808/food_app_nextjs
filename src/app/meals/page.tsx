7da1bdc6b9acbb0cd9aab363b1caeccd41aaa783
leviettien1808 <leviettien1808@gmail.com>
2025-10-23 15:43:16 +0700

    [page] Update: Meals content

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -1,7 +1,28 @@
+import classes from "@/app/meals/page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import Link from "next/link";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Những bữa ăn ngon do
+          <span className={classes.highlight}>chính bạn</span>
+          tạo ra
+        </h1>
+        <p>
+          Chọn công thức yêu thích của bạn và tự nấu. Thật dễ dàng và thú vị!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Chia sẻ công thức nấu ăn yêu thích của bạn
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
